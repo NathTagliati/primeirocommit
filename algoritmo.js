e563107dd75035aee47b52e39b75cf2a6422339e
nathali <nathali.souza.fumcad@despertar.org.br>
2023-03-07 16:17:41 -0300

    primeiro commit

---
+++ algoritmo.js
@@ -0,0 +1,14 @@
+var numeroUm=0
+let numeroDois=10
+let numeroTress=20
+
+//As variáveis de const não podem ser atualizadas nem declaradas novamente enquanto as de var podem ser declarados sem serem inicializadas, variáveis declaradas com const devem ser inicializadas no momento da declaração.podem ser declarados sem serem inicializadas, variáveis declaradas com const devem ser inicializadas no momento da declaração.
+
+console.log(numeroUm)
+//imprime o texto no console como uma mensagem de log
+
+numeroUm=numeroDois;
+console.log(numeroUm)
+
+numeroUm=numeroTres;
+console.log(numeroUm);
